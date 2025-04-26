64513505933da2c9b63f956f1f05ffea2d9e315f
thisispraphullverma@gmail.com <thisispraphullverma@gmail.com>
2025-04-26 22:27:57 +0530

    added linear Search for character in string

--- src/linearsearch.java
+++ src/linearsearch.java
@@ -12,8 +12,22 @@ public class linearsearch {
 
     }
 
+    static int LinearSearchString(char a, String s){
+        for (int i = 0; i < s.length(); i++){
+            if(a==s.charAt(i)){
+                return 1;
+            }
+
+
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Element is present at: " + LinearSearch(1, new int[]{13, 42, 14, 53, 6, 4, 2, 64}));
+        System.out.println(LinearSearch(1, new int[]{13, 42, 14, 53, 6, 4, 2, 64}));
+        System.out.println(LinearSearchString('s', "Hello worlds."));
+
 
     }
 }
